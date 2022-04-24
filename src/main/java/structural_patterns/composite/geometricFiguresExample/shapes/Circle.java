47de2bf64951044a1d3ccee315c498e8d8b31f6a
Daria Supińska <dariasupinska@gmail.com>
2022-04-24 22:36:43 +0200

    Composite pattern: circle shape added

---
+++ src/main/java/structural_patterns/composite/geometricFiguresExample/shapes/Circle.java
@@ -0,0 +1,28 @@
+package structural_patterns.composite.geometricFiguresExample.shapes;
+
+import java.awt.*;
+
+public class Circle extends BaseShape {
+    public int radius;
+
+    public Circle(int x, int y, Color color, int radius) {
+        super(x, y, color);
+        this.radius = radius;
+    }
+
+    @Override
+    public int getWidth() {
+        return radius * 2;
+    }
+
+    @Override
+    public int getHeight() {
+        return radius * 2;
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        graphics.drawOval(x, y, getWidth() - 1, getHeight() - 1);
+    }
+}
