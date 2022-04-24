4d3cc70ec54caede9257ad0b474fda5ae0b4cd8f
Daria Supińska <dariasupinska@gmail.com>
2022-04-24 22:33:31 +0200

    Composite pattern: Dot shape added

--- src/main/java/structural_patterns/composite/geometricFiguresExample/shapes/Dot.java
+++ src/main/java/structural_patterns/composite/geometricFiguresExample/shapes/Dot.java
@@ -1,4 +1,28 @@
 package structural_patterns.composite.geometricFiguresExample.shapes;
 
-public class Dot {
+import java.awt.*;
+
+public class Dot extends BaseShape {
+
+    private final int DOT_SIZE = 3;
+
+    public Dot(int x, int y, Color color) {
+        super(x, y, color);
+    }
+
+    @Override
+    public int getWidth(){
+        return DOT_SIZE;
+    }
+
+    @Override
+    public int getHeight(){
+        return DOT_SIZE;
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        graphics.fillRect(x-1, y-1, getWidth(), getHeight());
+    }
 }
