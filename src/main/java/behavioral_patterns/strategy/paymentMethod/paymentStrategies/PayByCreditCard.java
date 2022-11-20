cf5a0a68bb4caac3944b5a35574712c78afd2f73
Daria Supińska <dariasupinska@gmail.com>
2022-11-20 11:54:16 +0100

    CreditCard's strategy implemented

--- src/main/java/behavioral_patterns/strategy/paymentMethod/paymentStrategies/PayByCreditCard.java
+++ src/main/java/behavioral_patterns/strategy/paymentMethod/paymentStrategies/PayByCreditCard.java
@@ -1,15 +1,40 @@
 package behavioral_patterns.strategy.paymentMethod.paymentStrategies;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
 
-public class PayByCreditCard implements IPayStrategy{
-    @Override
-    public boolean pay(BigDecimal paymentAmout) {
-        return false;
-    }
+public class PayByCreditCard implements IPayStrategy {
+    private final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+    private CreditCard creditCard;
 
     @Override
     public void collectPaymentDetails() {
+        try {
+            System.out.print("Enter the card number: ");
+            String number = READER.readLine();
+            System.out.print("Enter the card expiration date 'mm/yy': ");
+            String date = READER.readLine();
+            System.out.print("Enter the CVV code: ");
+            String cvv = READER.readLine();
+            creditCard = new CreditCard(number, date, cvv);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public boolean pay(BigDecimal paymentAmout) {
+        if (cardIsPresent()) {
+            System.out.println("Paying " + paymentAmout + " using Credit Card");
+            creditCard.setAmount(creditCard.getAmount() - paymentAmout);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean cardIsPresent() {
+        return creditCard != null;
     }
 }
