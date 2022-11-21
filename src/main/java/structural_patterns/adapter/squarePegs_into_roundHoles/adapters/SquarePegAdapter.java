a59be40a24fa59afe3f41c1344766daa3ed2322b
Daria Supińska <dariasupinska@gmail.com>
2022-11-21 21:57:34 +0100

    SquarePageAdapter class adjusted

--- src/main/java/structural_patterns/adapter/squarePegs_into_roundHoles/adapters/SquarePegAdapter.java
+++ src/main/java/structural_patterns/adapter/squarePegs_into_roundHoles/adapters/SquarePegAdapter.java
@@ -1,4 +1,21 @@
 package structural_patterns.adapter.squarePegs_into_roundHoles.adapters;
 
-public class SquarePegAdapter {
+import structural_patterns.adapter.squarePegs_into_roundHoles.round.RoundPeg;
+import structural_patterns.adapter.squarePegs_into_roundHoles.square.SquarePeg;
+
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+
+        // Calculate a minimum circle radius, which can fit this peg.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
 }
