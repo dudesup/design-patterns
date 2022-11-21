04c6742b345acfbc62df6df00db26c601f9a17a3
Daria Supińska <dariasupinska@gmail.com>
2022-11-21 21:53:51 +0100

    Round classes adjusted

--- src/main/java/structural_patterns/adapter/squarePegs_into_roundHoles/round/RoundHole.java
+++ src/main/java/structural_patterns/adapter/squarePegs_into_roundHoles/round/RoundHole.java
@@ -1,4 +1,19 @@
 package structural_patterns.adapter.squarePegs_into_roundHoles.round;
 
 public class RoundHole {
+    private double radius;
+
+    public RoundHole(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public boolean fits(RoundPeg peg) {
+        boolean result;
+        result = (this.getRadius() >= peg.getRadius());
+        return result;
+    }
 }
--- src/main/java/structural_patterns/adapter/squarePegs_into_roundHoles/round/RoundPeg.java
+++ src/main/java/structural_patterns/adapter/squarePegs_into_roundHoles/round/RoundPeg.java
@@ -1,4 +1,16 @@
 package structural_patterns.adapter.squarePegs_into_roundHoles.round;
 
 public class RoundPeg {
+    private double radius;
+
+    public RoundPeg() {
+    }
+
+    public RoundPeg(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
 }
