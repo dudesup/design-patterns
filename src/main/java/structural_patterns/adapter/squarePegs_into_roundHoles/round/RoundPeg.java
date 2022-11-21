1366471711d31656005eae5c7ab11710db816638
Daria Supińska <dariasupinska@gmail.com>
2022-11-21 21:46:12 +0100

    Classes added

---
+++ src/main/java/structural_patterns/adapter/squarePegs_into_roundHoles/round/RoundPeg.java
@@ -0,0 +1,4 @@
+package structural_patterns.adapter.squarePegs_into_roundHoles.round;
+
+public class RoundPeg {
+}
---
+++ src/main/java/structural_patterns/adapter/squarePegs_into_roundHoles/square/SquarePeg.java
@@ -0,0 +1,4 @@
+package structural_patterns.adapter.squarePegs_into_roundHoles.square;
+
+public class SquarePeg {
+}
