9da2f2323e86b36722276f1d1eac2dd68ba85a53
Daria Supińska <dariasupinska@gmail.com>
2022-11-24 22:17:33 +0100

    EmailNotificationListener  class added

---
+++ src/main/java/behavioral_patterns/observer/Demo.java
@@ -0,0 +1,20 @@
+package behavioral_patterns.observer;
+
+import behavioral_patterns.observer.editor.Editor;
+import behavioral_patterns.observer.listeners.EmailNotificationListener;
+import behavioral_patterns.observer.listeners.LogOpenListener;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
+
+        try {
+            editor.openFile("test.txt");
+            editor.saveFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
