c9a361e783da95fff3f551ea5cebf177dd4c770d
Daria Supińska <dariasupinska@gmail.com>
2023-06-27 23:26:44 +0200

    decorator: adjusted

---
+++ src/main/java/structural_patterns/decorator/Demo.java
@@ -0,0 +1,26 @@
+package structural_patterns.decorator;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.logging.Logger;
+
+@Slf4j
+public class Demo {
+    public static void main(String[] args) {
+        Logger LOGGER = Logger.getLogger(Demo.class.getName());
+
+        String salaryRecords = "Name,Salary\nJohn Smith,100000\nSteven Jobs,912000";
+        DataSourceDecorator encoded = new CompressionDecorator(
+                new EncryptionDecorator(
+                        new FileDataSource("out/OutputDemo.txt")));
+        encoded.writeData(salaryRecords);
+        DataSource plain = new FileDataSource("out/OutputDemo.txt");
+
+        LOGGER.info("- Input ----------------");
+        LOGGER.info(salaryRecords);
+        LOGGER.info("- Encoded --------------");
+        LOGGER.info(plain.readData());
+        LOGGER.info("- Decoded --------------");
+        LOGGER.info(encoded.readData());
+    }
+}
